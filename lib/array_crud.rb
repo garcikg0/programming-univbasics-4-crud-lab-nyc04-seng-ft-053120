9ba9fd8a9f063a15aa7d9213094fb585d956d9e1
Kevin Garcia <kg.gar91@gmail.com>
2020-06-09 18:17:54 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,10 +6,8 @@ def create_an_array
   [1,2,3,4]
 end
 
-def add_element_to_end_of_array(array, element)
-  [1,2,3,4]
-  add_element_to_end_of_array.push(5)
-  p add_element_to_end_of_array
+def add_element_to_end_of_array(food, "meats")
+  food.push("cheese")
 end
 
 def add_element_to_start_of_array(array, element)
