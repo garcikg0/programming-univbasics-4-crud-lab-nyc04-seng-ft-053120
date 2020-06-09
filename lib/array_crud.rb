53b513f0387aac992dc373c64e4d3fc4aa73dbef
Kevin Garcia <kg.gar91@gmail.com>
2020-06-09 18:14:30 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -4,12 +4,11 @@ end
 
 def create_an_array
   [1,2,3,4]
-  create_an_array.push("5")
 end
 
 def add_element_to_end_of_array(array, element)
   [1,2,3,4]
-
+  add_element_to_end_of_array.push("5")
 end
 
 def add_element_to_start_of_array(array, element)
